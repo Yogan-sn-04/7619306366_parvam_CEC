0ef4469c6b06977b29afa0261fd4b6a29867b4be
unknown <yogansubramanyanaik@gmail.com>
2024-12-09 14:16:40 +0530

    ALL Files

--- arrays.c
+++ arrays.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    int size=0,a[100],n;
+    int size=0,a[100],i,n;
     scanf("%d",&size);
     for(i=0;i<size;scanf("%d",&a[i++]));
-    for(i=0;i<size;printf("%d",a[i++]));
+    for(i=0;i<size;printf("%d ",a[i++]));
 }
